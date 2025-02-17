29f4850a0f5b9bed8f54d771c6c18bd4c63b095e
kyrariedel <kyrariedel@gmail.com>
2025-02-17 18:07:23 -0500

    lab 3 majority of files - need to fix sm stuff on laptop and don't want corruption

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -1,7 +1,63 @@
+import VariablesAndConstants from "./variablesandconstants";
+import VariableTypes from "./variabletypes";
+import BooleanVariables from "./booleanvariables";
+import IfElse from "./ifelese";
+import TernaryOperator from "./ternaryoperator";
+import ConditionalOutputIfElse from "./conditionaloutputifelse";
+import ConditionalOutputInline from "./conditionaloutputinline";
+import LegacyFunctions from "./legacyfunctions";
+import ArrowFunctions from "./arrowfunctions";
+import ImpliedReturn from "./impliedreturn";
+import TemplateLiterals from "./templateliterals";
+import SimpleArrays from "./simplearrays";
+import ArrayIndexAndLength from "./arrayindexandlength";
+import AddingAndRemovingToFromArrays from "./addingandremovingtofromarrays";
+import ForLoops from "./forloops";
+import MapFunction from "./mapfunction";
+import FindFunction from "./findfunction";
+import FindIndex from "./findindex";
+import FilterFunction from "./filterfunction";
+import Destructing from "./destructing";
+import DestructingImports from "./destructingimports";
+import FunctionDestructing from "./functiondestructing";
+import Spreading from "./spreading";
+import House from "./house";
+import Classes from "./classes";
+import Styles from "./styles";
+import Add from "./add";
+
+
 export default function Lab3() {
-    return (
-      <div>
-        <h2>Lab 3</h2>
-      </div>
-  );}
-  
+  return(
+    <div id="wd-lab3">
+      <h3>Lab 3</h3>
+      <VariablesAndConstants/>
+      <VariableTypes/>
+      <BooleanVariables/>
+      <IfElse/>
+      <TernaryOperator/>
+      <ConditionalOutputIfElse/>
+      <ConditionalOutputInline/>
+      <LegacyFunctions/>
+      <ArrowFunctions/>
+      <ImpliedReturn/>
+      <TemplateLiterals/>
+      <SimpleArrays/>
+      <ArrayIndexAndLength/>
+      <AddingAndRemovingToFromArrays/>
+      <ForLoops/>
+      <MapFunction/>
+      <FindFunction/>
+      <FindIndex/>
+      <FilterFunction/>
+      <Destructing/>
+      <DestructingImports/>
+      <FunctionDestructing/>
+      <Spreading/>
+      <House/>
+      <Classes/>
+      <Styles/>
+      <Add a={3} b={4} />
+    </div>
+  );
+}
