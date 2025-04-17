85b5acaadc420792f642246a1ce49c007fc3f114
Kyra Riedel <kyrariedel@gmail.com>
2025-04-17 00:44:02 -0400

    comment

--- src/Kambaz/Courses/index.tsx
+++ src/Kambaz/Courses/index.tsx
@@ -3,7 +3,7 @@ import Modules from "./Modules";
 import Home from "./Home";
 import Assignments from "./Assignments";
 import PeopleTable from "./People/table";
-import AssignmentEditor from "./Assignments/editor";
+// import AssignmentEditor from "./Assignments/editor";
 import { Navigate, Route, Routes, useParams, useLocation } from "react-router";
 import { FaAlignJustify } from "react-icons/fa";
 import ProtectedRoute from "../Account/protectedroute";
